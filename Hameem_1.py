cf78bb47228040569ee8d1c992fb1588cdd638ce
MishQat <68779782+MishQat@users.noreply.github.com>
2021-06-13 13:25:30 +0600

    Create Hameem_1.py

---
+++ Hameem_1.py
@@ -0,0 +1,12 @@
+  
+Time = float(input("Enter hour: "))
+
+Rate = float(input("Enter rate: "))
+
+Payment = float(Time * Rate)
+
+if Time == 40 or Time > 40:
+    Payment = Payment * 1.5
+    print(Payment)
+else: 
+    print(Payment)
