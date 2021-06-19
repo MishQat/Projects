491ca5aadaea26acf249d7d7f76725743cf19d9a
MishQat <68779782+MishQat@users.noreply.github.com>
2021-06-19 22:40:33 +0600

    Update Panildrome.py

--- Panildrome.py
+++ Panildrome.py
@@ -1,5 +1,5 @@
 Input = input("Enter of what you want to check panildrome of: ")
-r_input=Input[::-1]
+r_input=Input[::-1]#reversed the string(main logic)
 if r_input == Input:
     print("Its a Panildrome")
 else:
