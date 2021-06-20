576877b81802c157ba869bf2670580d77ae73e21
MishQat <68779782+MishQat@users.noreply.github.com>
2021-06-20 18:05:55 +0600

    Update Diarycode.py

--- Personal_diary/Diarycode.py
+++ Personal_diary/Diarycode.py
@@ -10,15 +10,15 @@ year_today = time.strftime("%Y")#gets all data for time and date
 try:
     user_input_diary = input(f"Today is {day_today}, {date_today}th of {month_today} {year_today} \nWhat do you want to write today?... ")#asks user
 
-    f = open("Diary101.txt","a")
+    f = open("Diary.txt","a")
     f.write(f"\nEntry of {time_now}\n{user_input_diary}")#takes entry
     f.close
     user_input = input("New entry added succesfully, do you want to see your whole diary.y/n?... ")
-    f = open("Diary101.txt","r")
+    f = open("Diary.txt","r")
 except:
     print("Data entry unsuccesful, try again.")
     user_input = input("Do you wish to see your whole diary.y/n?... ")
-    f = open("Diary101.txt","r")
+    f = open("Diary.txt","r")
 if user_input == "y":
     print(f.read())#reads out the whole file if user wants to
 else:
