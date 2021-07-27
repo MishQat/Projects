e3e95d5132832c8233dcc7fa6220eb381d73f252
MishQat <68779782+MishQat@users.noreply.github.com>
2021-07-27 16:48:49 +0600

    Update Maincode.py

--- GUI_Calculator/Maincode.py
+++ GUI_Calculator/Maincode.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from typing import Sized
+
 
 def click(event):
     text = event.widget.cget("text")#Gets the data of the widget in this case the button that has been pressed 
@@ -8,7 +8,13 @@ def click(event):
         if Screenvalue.get().isdigit():#checks if the number on the calculator is digits or not
             value = float(Screenvalue.get())#calculates the number
         else:
-            value = eval(Screenvalue.get())#calculates the number 
+            try:
+                value = eval(Screenvalue.get())#calculates the number 
+            except Exception as e:
+                print(e)
+                Screenvalue.set("Error, Try again")
+                Frame1.update()
+           
         Screenvalue.set(value)#updates the answer
         Frame1.update()
     elif text == "C":
@@ -22,7 +28,7 @@ root = Tk()
 root.geometry("854x800")
 root.configure(bg="#00B4AB")
 root.title("Calculator ")
-root.wm_iconbitmap("calculator - Copy.ico")
+root.wm_iconbitmap("calculator.ico")
 sbarvar = StringVar()
 sbarvar.set("Calculator 1.0.0 by Ahmed Mishqat____________________________________________________________________________________________________________________hope you enjoy it.")
 sbar = Label(root,textvariable=sbarvar,relief= SUNKEN,anchor= "w",bg="#db3939")#status bar
@@ -32,7 +38,6 @@ Screenvalue.set("")
 Entryofuser = Entry(root,textvariable=Screenvalue,font= "lucida 40 bold",fg=  "#e6175c",background= "#000000").pack(fill=X, padx=20,pady=30)
 #TODO:We make the button which will print out theris coresponding number in the calculator
 #for number 1,2,3
-
 Frame1 = Frame(root,bg="#79c9bc")
 Buttonnum = Button(Frame1,text="1",fg="#000000",font="Lucida 35 bold",padx=22,pady=28,bg= "#0269A4")
 Buttonnum.pack(side=LEFT ,padx= 6, pady=5)
@@ -44,7 +49,6 @@ Buttonnum = Button(Frame1,text="3",fg="#000000",font="Lucida 35 bold",padx=22,pa
 Buttonnum.pack(side=LEFT ,padx= 6,  pady=5)
 Buttonnum.bind("<Button-1>",click)
 Frame1.pack( pady= 10)
-
 #for number 4,5,6
 Frame1 = Frame(root,bg="#79c9bc",)
 Buttonnum = Button(Frame1,text="4",fg="#000000",font="Lucida 35 bold",padx=22,pady=28,bg= "#0269A4")
@@ -57,8 +61,7 @@ Buttonnum = Button(Frame1,text="6",fg="#000000",font="Lucida 35 bold",padx=22,pa
 Buttonnum.pack(side=LEFT ,padx= 6,  pady=5)
 Buttonnum.bind("<Button-1>",click)
 Frame1.pack(pady= 10)
-
-#for number 9,8,7 and 0
+#for number 9,8,7
 Frame1 = Frame(root,bg="#79c9bc",)
 Buttonnum = Button(Frame1,text="7",fg="#000000",font="Lucida 35 bold",padx=22,pady=28,bg= "#0269A4")
 Buttonnum.pack(side=LEFT,padx= 6, pady=5)
