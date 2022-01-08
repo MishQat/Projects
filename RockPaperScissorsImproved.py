3103e9c52e06bdb77cfaeee4e50e98c00b32c81d
MishQat <68779782+MishQat@users.noreply.github.com>
2022-01-08 15:05:25 +0600

    Update RockPaperScissorsImproved.py

--- RockPaperScissorsImproved.py
+++ RockPaperScissorsImproved.py
@@ -2,16 +2,17 @@ import random,sys
 User_wins = 0
 User_looses = 0
 User_ties = 0
-rpc_ = ["Rock", "Paper", "Scissors"]
+rpc_ = ["rock", "paper", "scissors"]
 while True:#main loop
     random.shuffle(rpc_)#shuffler
     rpc_random = random.choice(rpc_)#randomiser
     print(f"wins {User_wins}, looses {User_looses}, ties {User_ties}")
     while True:
-        user_input = str(input("Rock, Paper or Pcissors, press (q)uit to quit... "))
+        user_input = str(input("rock, paper or scissors, press (q)uit to quit... "))
+        user_input = user_input.lower()
         if user_input == "q":
             sys.exit()#exits if user dosent wants to play
-        if user_input =="Rock" or user_input == "Paper" or user_input == "Scissors":
+        if user_input =="rock" or user_input == "paper" or user_input == "scissors":
             break#user picks what he wants to play
         else:
             print("invalid input try again... ")#for invalid input
@@ -22,27 +23,27 @@ while True:#main loop
         print(f"Computer played {rpc_random}")
         print("Draw!")
         User_ties = User_ties + 1
-    elif user_input == "Rock" and rpc_random == "Paper":
+    elif user_input == "rock" and rpc_random == "paper":
         print(f"Computer played {rpc_random}")
         print("User won!")
         User_wins = User_wins + 1
-    elif user_input =="Rock" and rpc_random == "Scissors":
+    elif user_input =="rock" and rpc_random == "scissors":
         print(f"Computer played {rpc_random}")
         print("Computer won!")
         User_looses = User_looses +1
-    elif user_input == "Paper" and rpc_random == "Rock":
+    elif user_input == "paper" and rpc_random == "rock":
         print(f"Computer played {rpc_random}")
         print("Computer won!")
         User_looses = User_looses + 1
-    elif user_input == "Paper" and rpc_random == "Scissors":
+    elif user_input == "paper" and rpc_random == "scissors":
         print(f"Computer played {rpc_random}")
         print("User won!")
         User_wins = User_wins +1
-    elif user_input =="Scissors" and rpc_random == "Rock":
+    elif user_input =="scissors" and rpc_random == "rock":
         print(f"Computer played {rpc_random}")
         print("User won!")
         User_wins = User_wins+1
-    elif user_input == "Scissors" and rpc_random == "Paper":
+    elif user_input == "scissors" and rpc_random == "paper":
         print(f"Computer played {rpc_random}")
         print("Computer won!")
         User_looses = User_looses+1
